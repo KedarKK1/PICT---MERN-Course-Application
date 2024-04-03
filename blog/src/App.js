71b4653683b97af37cc437af21849e2425a588d7
Jay Naphade <jayn1322402@gmail.com>
2024-04-03 17:54:03 +0530

    backend linked to login page

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,13 +7,14 @@ import {
 import Navbar from './components/Navbar';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import Blog from './components/Blog';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path='/' element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
--- blog/src/pages/LoginPage.js
+++ blog/src/pages/LoginPage.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useNavigate} from "react-router-dom"
 
 const LoginPage = () => {
+    let navigate = useNavigate();
+    const [credentials, setCredentials] = useState({email: "", password:""});
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault();
+        const respose = await fetch(`http://localhost:3001/auth/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({email: credentials.email, password: credentials.password})
+        });
+        const json = await respose.json();
+        localStorage.setItem('token', json.authToken);
+        localStorage.setItem('userId', json.userId);
+        navigate('/', {replace:true});
+        
+    }
+    const onChange = (e) => {
+        setCredentials({...credentials, [e.target.name] : e.target.value});
+    }
     return (
         <div className='container' style={{ width: "clamp(250px, 40%, 600px)", border: "1px solid black", borderRadius: "10px", padding: "20px", marginTop:"50px" }}>
             <h3>Login</h3>
             <hr />
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" value={credentials.email} className="form-control" onChange={onChange} id="email" aria-describedby="emailHelp" name="email" />
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" value={credentials.password} onChange={onChange} className="form-control" id="password" name="password" />
                 </div>
                 <button style={{width:"100%"}} type="submit" className="btn btn-primary">Submit</button>
             </form>
